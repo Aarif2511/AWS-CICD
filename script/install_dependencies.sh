972d9505d2a212e5d00b211785192fc4dda8eea3
Aarif2511 <mohamedaarif.35@gmail.com>
2025-01-30 04:16:39 +0530

    final changes

--- script/install_dependencies.sh
+++ script/install_dependencies.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sudo yum install http -y
-sudo start httpd start
+# Install dependencies
+sudo yum update -y
+sudo yum install -y httpd php mysql
+
+# Start and enable Apache web server
+sudo systemctl start httpd
+sudo systemctl enable httpd
+
+# Exit with success status
+exit 0
